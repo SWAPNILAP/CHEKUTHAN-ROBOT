6075d0bbc7cc54e565f7873bde9bcab920f194cc
SWAPNILAP <132188201+SWAPNILAP@users.noreply.github.com>
2023-05-07 17:51:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from pyrogram import Client, filters
 
 
-API_iD = ""
-API_HASH = ""
-BOT_TOKEN = ""
+API_iD = "12360947"
+API_HASH = "3dddbb5be694212e7954f7fdd5960a24"
+BOT_TOKEN = "6090699116:AAGrrAb1KYsuP5bOvVsPwEHoNENhmpRdpC4"
 
 
 BOT = Client(
