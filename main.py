517c92a8ed4fa675c9f4c57ecff3b6d6213b83c3
SWAPNILAP <132188201+SWAPNILAP@users.noreply.github.com>
2023-05-07 17:35:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+from pyrogram import Client, filters
 
+
+API_iD = ""
+API_HASH = ""
+BOT_TOKEN = ""
+
+
+BOT = Client(
+    name="Telegram bot"
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN
+)
